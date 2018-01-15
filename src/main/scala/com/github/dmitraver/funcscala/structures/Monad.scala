72656e59b015f3ae385c334909efc6a2af843225
Dmitry Avershin <dmitraver@gmail.com>
2018-01-15 17:54:31 +0100

    Implements sequence, traverse and replicate for monad.

--- src/main/scala/com/github/dmitraver/funcscala/structures/Monad.scala
+++ src/main/scala/com/github/dmitraver/funcscala/structures/Monad.scala
@@ -10,6 +10,23 @@ trait Monad[F[_]] extends Functor[F]{
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
     flatMap(fa)(a => map(fb)(b => f(a,b)))
   }
+
+  def sequence[A](lma: List[F[A]]): F[List[A]] = lma match {
+    case Nil => unit(Nil)
+    case x :: xs => flatMap(x)(a => map(sequence(xs))(b => a :: b))
+  }
+
+  def traverse[A, B](lma: List[A])(f: A => F[B]): F[List[B]] = lma match {
+    case Nil => unit(Nil)
+    case x :: xs => flatMap(f(x))(a => map(traverse(xs)(f))(b => a :: b))
+  }
+
+  def replicateM[A](n: Int, ma: F[A]): F[List[A]] = {
+    if (n == 0) unit(Nil)
+    else {
+      flatMap(ma)(a => map(replicateM(n - 1, ma))(b => a :: b))
+    }
+  }
 }
 
 object OptionMonad extends Monad[Option] {
@@ -25,5 +42,21 @@ object StreamMonad extends Monad[Stream] {
 object ListMonad extends Monad[List] {
   override def unit[A](a: => A): List[A] = List(a)
   override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa flatMap f
+}
+
+object MonadApplication {
+  def main(args: Array[String]): Unit = {
+    // sequence
+    println(OptionMonad.sequence(List(Some(1), Some(2), Some(3))))
+    println(OptionMonad.sequence(List(Some(1), None, Some(3))))
+
+    // traverse
+    println(OptionMonad.traverse(List(1, 2, 3))(a => Some(a)))
+    println(OptionMonad.traverse(List(1, 2, 3))(a => if (a == 2) None else Some(a)))
+
+    //replicate
+    println(OptionMonad.replicateM(5, Some(1)))
+    println(OptionMonad.replicateM(5, None))
+  }
 }
 
