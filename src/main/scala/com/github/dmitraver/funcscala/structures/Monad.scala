f293be91ac27c547b5b37aa3130cf21ec6419ab6
Dmitry Avershin <dmitraver@gmail.com>
2018-01-18 16:35:25 +0100

    Implements product and filterM for monad.

--- src/main/scala/com/github/dmitraver/funcscala/structures/Monad.scala
+++ src/main/scala/com/github/dmitraver/funcscala/structures/Monad.scala
@@ -27,6 +27,13 @@ trait Monad[F[_]] extends Functor[F]{
       flatMap(ma)(a => map(replicateM(n - 1, ma))(b => a :: b))
     }
   }
+
+  def product[A,B](ma: F[A], mb: F[B]): F[(A, B)] = map2(ma, mb)((_, _))
+
+  def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] = ms match {
+    case Nil => unit(Nil)
+    case x :: xs => flatMap(f(x))(b => if (b) map(filterM(xs)(f))(y => x :: y) else filterM(xs)(f))
+  }
 }
 
 object OptionMonad extends Monad[Option] {
@@ -54,9 +61,16 @@ object MonadApplication {
     println(OptionMonad.traverse(List(1, 2, 3))(a => Some(a)))
     println(OptionMonad.traverse(List(1, 2, 3))(a => if (a == 2) None else Some(a)))
 
-    //replicate
+    //replicate option
     println(OptionMonad.replicateM(5, Some(1)))
     println(OptionMonad.replicateM(5, None))
+
+    //replicate list
+    println(ListMonad.replicateM(5, List(1, 2)))
+    println(ListMonad.replicateM(5, Nil))
+
+    // filterM
+    println(OptionMonad.filterM(List(1, 2, 3, 4, 5, 6))(x => Some(x % 2 == 0)))
   }
 }
 
