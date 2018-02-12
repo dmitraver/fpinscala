ccc89ffef965ac3eebd91f45f01bfcb9abeac889
Dmitry Avershin <dmitraver@gmail.com>
2018-02-12 21:56:11 +0100

    Implements reverse and foldLeft.

--- src/main/scala/com/github/dmitraver/funcscala/structures/Traverse.scala
+++ src/main/scala/com/github/dmitraver/funcscala/structures/Traverse.scala
@@ -66,7 +66,15 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
   }
 
   def toList[A](fa: F[A]): List[A] = {
-    mapAccum(fa, List[A]())((a, s) => (a :: s, s))._2.reverse
+    mapAccum(fa, List[A]())((a, s) => ((), a :: s))._2.reverse
+  }
+
+  def reverse[A](fa: F[A]): F[A] = {
+    mapAccum(fa, toList(fa).reverse)((a, s) => (s.head, s.tail))._1
+  }
+
+  override def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B = {
+    mapAccum(as, z)((a, s) => ((), f(s, a)))._2
   }
 }
 
@@ -75,6 +83,8 @@ object ListTraverse extends Traverse[List] {
     val AG = implicitly[Applicative[G]]
     fa.foldRight(AG.unit(Nil: List[B]))((a, acc) => AG.map2(f(a), acc)(_ :: _))
   }
+
+  override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = ???
 }
 
 object OptionTraverse extends Traverse[Option] {
@@ -82,6 +92,8 @@ object OptionTraverse extends Traverse[Option] {
     val AG = implicitly[Applicative[G]]
     fa.foldRight(AG.unit(None: Option[B]))( (a, acc) => AG.map2(f(a),acc)((a, b) => Some(a)))
   }
+
+  override def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B): B = ???
 }
 
 case class Tree_[+A](head: A, tail: List[Tree_[A]])
@@ -90,6 +102,8 @@ object TreeTraverse extends Traverse[Tree_] {
     val AG = implicitly[Applicative[G]]
     AG.map2(f(fa.head), ListTraverse.traverse(fa.tail)(tree => traverse(tree)(f)))((a, b) => Tree_(a, b))
   }
+
+  override def foldRight[A, B](as: Tree_[A])(z: B)(f: (A, B) => B): B = ???
 }
 
 
@@ -99,6 +113,7 @@ object TraverseApplication {
     println(OptionTraverse.map(None: Option[Int])(_ * 2))
     println(ListTraverse.map(List(1, 2, 3, 4, 5))(_ * 2))
 
-
+    println(ListTraverse.reverse(List(1,2,3,4,5,6)))
+    println(ListTraverse.foldLeft(List(1,2,3))(0)(_ + _))
   }
 }
