95558afce1bfac673dc3edf5a6562c16472b86a7
Dmitry Avershin <dmitraver@gmail.com>
2018-02-15 10:25:13 +0100

    Implements map/flatmap for for-comprehensions.

--- src/main/scala/com/github/dmitraver/funcscala/io/IO.scala
+++ src/main/scala/com/github/dmitraver/funcscala/io/IO.scala
@@ -10,6 +10,12 @@ trait IO[A] { self =>
       io.run
     }
   }
+
+  def map[B](f: A => B): IO[B] = new IO[B] {
+    override def run: B = f(self.run)
+  }
+
+  def flatMap[B](f: (A) => IO[B]): IO[B] = f(self.run)
 }
 
 object IO extends Monad[IO] {
@@ -19,16 +25,31 @@ object IO extends Monad[IO] {
 
   override def flatMap[A, B](fa: IO[A])(f: (A) => IO[B]): IO[B] = f(fa.run)
 
-
   def empty = new IO[Unit] {
     override def run: Unit = ()
   }
 }
 
 object IOApplication {
-  def PrintLine(msg: String): IO[Unit] = {
-    new IO {
+  def printLine(msg: String): IO[Unit] = {
+    new IO[Unit] {
       override def run: Unit = println(msg)
     }
   }
+
+  def readLine: IO[String] = {
+    IO.unit(scala.io.StdIn.readLine())
+  }
+
+  def main(args: Array[String]): Unit = {
+    val greet: IO[Unit] = for {
+      _      <- printLine("Please enter your name")
+      name   <- readLine
+      _      <- printLine(s"Thank you $name!")
+    } yield ()
+
+    greet.run
+  }
+
+
 }
