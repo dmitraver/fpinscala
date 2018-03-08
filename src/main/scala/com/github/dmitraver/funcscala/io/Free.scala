978e61b5d999179f39195c08a8dc43d457257293
Dmitry Avershin <dmitraver@gmail.com>
2018-03-08 12:32:19 +0100

    Implements free monad.

---
+++ src/main/scala/com/github/dmitraver/funcscala/io/Free.scala
@@ -0,0 +1,43 @@
+package com.github.dmitraver.funcscala.io
+
+import com.github.dmitraver.funcscala.structures.Monad
+
+object Free {
+  sealed trait Free[F[_], A] {
+    def flatMap[B](f: A => Free[F, B]): Free[F, B] = FlatMap(this, f)
+    def map[B](f: A => B): Free[F, B] = {
+      flatMap(f andThen(Return(_)))
+    }
+  }
+
+  case class Return[F[_], A](a: A) extends Free[F, A]
+  case class Suspend[F[_], A](s: F[A]) extends Free[F, A]
+  case class FlatMap[F[_], A, B](s: Free[F, A], f: A => Free[F, B]) extends Free[F, B]
+
+
+  object FreeMonad {
+    def freeMonad[F[_]]: Monad[({type f[x] = Free[F, x]})#f] = new Monad[({type f[x] = Free[F, x]})#f] {
+      override def unit[A](a: => A): Free[F, A] = Return(a)
+      override def flatMap[A, B](fa: Free[F, A])(f: A => Free[F, B]): Free[F, B] = fa flatMap f
+    }
+
+  }
+
+  @annotation.tailrec
+  def runTrampoline[A](a: Free[Function0[A], A]): A = a match {
+    case Return(a) => a
+    case Suspend(s) => s()
+    case FlatMap(x, f) => x match {
+      case Return(a) => f(a)
+      case Suspend(s) => f(s())
+      case FlatMap(y, g) => runTrampoline(y flatMap(a => g(a) flatMap f))
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+
